d778c2961e7608663ad60ac41d5ae64898ad8dac
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-08-12 00:32:19 +0100

    user db setup

--- src/__init__.py
+++ src/__init__.py
@@ -6,18 +6,20 @@ from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 
 def register_blueprint(app):
     from src.routes.general import general_blueprint
-    from src.routes.users import users_blueprint
+    from src.routes.users import user
     app.register_blueprint(general_blueprint)
-    app.register_blueprint(users_blueprint)
+    app.register_blueprint(user)
 
 
 register_blueprint(app)
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,30 +1,31 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src.models import User
 from flask_jwt_extended import create_access_token, jwt_required
+from src import app, db
 
-users_blueprint = Blueprint('users', __name__)
+user = Blueprint('users', __name__)
 
-@app.route('/signup', methods=['POST'])
+@user.route('/signup', methods=['POST'])
 def signup():
     print('YESHSHSH')
-    data = request.get_json()
+    data = request.json
     print(data)
-    new_user = User(data)
+    new_user = User(**data)
     new_user.set_password(data['password'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"message": "User created!"}), 201
 
-@app.route('/login', methods=['POST'])
+@user.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
+    data = request.json
     user = User.query.filter_by(email=data['email']).first()
     if user and user.check_password(data['password']):
         token = create_access_token(identity=user.email)
         return jsonify(access_token=token), 200
     return jsonify({"message": "Invalid credentials"}), 401
 
-@app.route('/protected', methods=['GET'])
+@user.route('/protected', methods=['GET'])
 @jwt_required()
 def protected_route():
     return jsonify({"message": "This is a protected route!"})
