02f83992277171e22870cdd4185905d408332d73
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-08-12 20:21:11 +0100

    expired and expiring soon;

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
 from src.models import User
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, create_refresh_token
 from src import app, db
+from datetime import timedelta
 
 user = Blueprint('users', __name__)
 
@@ -16,18 +17,24 @@ def signup():
     db.session.commit()
     return jsonify({"message": "User created!"}), 201
 
+@user.route('/token/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    current_user = get_jwt_identity()
+    access_token = create_access_token(identity=current_user, expires_delta=timedelta(days=1))
+    return jsonify(access_token=access_token), 200
+
 @user.route('/login', methods=['POST'])
 def login():
     data = request.json
     user = User.query.filter_by(email=data['email']).first()
     if user and user.check_password(data['password']):
-        token = create_access_token(identity=user.email)
-        print(jsonify(access_token=token, 
-            user_id=user.id, 
-            name=user.fullname, 
-            email=user.email))
+        token = create_access_token(identity=user.email, expires_delta=timedelta(days=1))
+        refresh_token = create_refresh_token(identity=user.email)
+
         return jsonify(
-            access_token=token, 
+            access_token=token,
+            refresh_token=refresh_token,
             user_id=user.id, 
             name=user.fullname, 
             email=user.email
